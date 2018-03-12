283dddc6f5840817e450bb1e0dc4c0fdbffca4f2
lucyL <lucylaurentino@gmail.com>
2018-03-13 11:50:50 +1300

    more commit

--- script.js
+++ script.js
@@ -17,14 +17,39 @@ let GreetingArrow = () => {
 //GreetingArrow
 
 
-function draw(){
+function draw(x, y, r) {
 
     ctx.beginPath()
-    ctx.arc(200, 200, 80, Math.PI * 2, false)
+    ctx.arc(x, y, r, Math.PI * 2, false)
     ctx.fillStyle = '#09c'
     ctx.strokeStyle = "black"
     ctx.stroke()
     ctx.fill()
 
 }
-draw()
+function animate() {
+
+    let x = Math.floor((Math.random() * 350))
+    let y = Math.floor((Math.random() * 350))
+
+    let r = Math.floor((Math.random() * 50))
+
+    let dx = Math.random() - 0.5
+    let dy = Math.random() - 0.5
+
+    setTimeout(function () {
+
+        requestAnimationFrame(animate)
+        ctx.clearRect(0, 0, 400, 400)
+    }, 200)
+
+    update(x, y, dx, dy, r)
+
+}
+function update(x, y, dx, dy, r) {
+    x += dx
+    y += dy
+
+    draw(x, y, r) //calling draw function
+}
+animate()
